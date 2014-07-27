92a7f58768cd126a0f8a7bb6dc5e3773ff7f72a0
sky <freelingchang@gmail.com>
2014-07-27 14:54:58 +0800

    x

--- forward.py
+++ forward.py
@@ -19,43 +19,48 @@ def start():
             #print buf
             print '收到请求 --- > http://',headers_data_g.group(1),headers_data_g.group(2)
             print '开始转发....'
-            try:
-                hostname = getHost(headers_data)
-                #hostname = getHost(headers)
-                hostip = getIp(hostname)
-                ###遇到302重定向无法转发的问题
-                buf = forward(hostip,headers_data)
-                #buf = forward(hostip,headers)
-                print "开始返回数据"
-                if buf and buf != 1:
-                    buf_len = len(buf)
-                    print '返回给client的长度为%d' % buf_len
-                    print '返回给client header'
-                    connection.sendall(buf)
-                    #connection.sendall(buf[1])
-                    #本来此处header和body应该分开返回，待后续处理
-                    #print '返回给client data'
-                    #if cbuf_all !='':
-                    #    connection.send(cbuf_all[1])
-                    #time.sleep(5)
-            except Exception,ex:
-                print Exception,":",ex
+            hostname = get_hostname(headers_data)
+            #hostname = getHost(headers)
+            hostip = get_ip(hostname)
+            if hostip == 1:
+                connection.sendall('域名无法解析')
+                connection.close
+                return 1
+            ###遇到302重定向无法转发的问题
+            buf = forward(hostip,headers_data)
+            #buf = forward(hostip,headers)
+            print "开始返回数据"
+            if buf and buf != 1:
+                buf_len = len(buf)
+                print '返回给client的长度为%d' % buf_len
+                print '返回给client header'
+                connection.sendall(buf)
+                #connection.sendall(buf[1])
+                #本来此处header和body应该分开返回，待后续处理
+                #print '返回给client data'
+                #if cbuf_all !='':
+                #    connection.send(cbuf_all[1])
+                #time.sleep(5)
+                print '发送完，关闭socket'
+                connection.close()  
+                #sys.exit(0)
         else:
             print '没发现请求头'
-    print '发送完，关闭socket'
-    connection.close()  
-    #sys.exit(0)
+            connection.close()  
         
 ##提取主机名
-def getHost(buf):
+def get_hostname(buf):
     dhost_g=re.search('Host: (.*)\r\n',buf)
-    if dhost_g:
+    try:
         hostname=dhost_g.group(1)
         #result = socket.getaddrinfo(hostname,None)
         print '获取请求的主机名成功'
         return hostname
+    except Exception,ex:
+        print Exception,":",ex
+        return 1
 ##解析ip
-def getIp(dhost):
+def get_ip(dhost):
     try:
         result = socket.getaddrinfo(dhost,None)
         if result:
@@ -64,6 +69,7 @@ def getIp(dhost):
             return hostip
     except Exception,ex:
         print Exception,":",ex
+        return 1
 def forward(hostip,headers):
     cl = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
     cl.connect((hostip,80))
@@ -85,63 +91,62 @@ def forward(hostip,headers):
         return 1
     if buf:
         print '第一次数据接收成功'
-        header_len=len(buf)
-        send_header_g = re.search("([\S\s]+)\r\n\r\n",buf)
+        proxy_first_data_len=len(buf)
+        proxy_header_g = re.search("([\S\s]+)\r\n\r\n",buf)
         #send_header_g = re.search("([\S\s]+)\r\n\r\n([\S\s]+)",buf)
-        if send_header_g:
-            send_header = send_header_g.group(1)
+        try:
+            proxy_header = proxy_header_g.group(1)
             #print send_header
-            header_len_header = len(send_header)+4
-            #print header_len_header
-        send_body_g = re.search("([\S\s]+)\r\n\r\n([\S\s]+)",buf)
-        send_first_body = ''
-        body_len_g=re.search("Content-Length: (.*)\r\n",buf)
+            proxy_header_len = len(proxy_header)+4
+            #print proxy_proxy_first_data_len
+        except Exception,ex:
+            print Exception,":",ex
+            print 'not found header'
+            cl.close()
+            return 1
+        proxy_first_body_g = re.search("([\S\s]+)\r\n\r\n([\S\s]+)",buf)
+        proxy_first_body = ''
+        proxy_body_len_g=re.search("Content-Length: (.*)\r\n",buf)
         #第一次接收中，就带有主体信息的
         #第一次接收中，只有纯头部信息，头部信息中带有length的.没有主体信息,则send_first_body 为空
-        if send_body_g:
-            send_first_body = send_body_g.group(2)
-        if body_len_g:
-            body_len=int(body_len_g.group(1))
-            if body_len:
-                print '第一次接收总长度为:',header_len
-                print '主体长度 %d' % body_len
+        if proxy_first_body_g:
+            proxy_first_body = proxy_first_body_g.group(2)
+        proxy_first_body_len = len(proxy_first_body)
+        if proxy_body_len_g:
+            proxy_body_len=int(proxy_body_len_g.group(1))
+            if proxy_body_len:
+                print '第一次接收总长度为:',proxy_first_data_len
+                print '主体长度 %d' % proxy_body_len
                 print '获取长度成功，开始接收全部数据...'
                 #带长度的，头和主体一起接收成功的，主体太短，只需要接收一次的
-                need = header_len-(header_len-header_len_header)+body_len
-                if header_len-header_len_header == body_len:
+                need = proxy_body_len-proxy_first_body_len+proxy_first_data_len
+                if proxy_first_body_len == proxy_body_len:
                     return buf
                 #带长度的，头和主体一起接收成功的，主体较长，需要循环多次接收的
                 try:
-                    s  = re.search("\r\n\r\n(.*)",buf)
-                    if s:
-                        body_in_header_len = len( s.group(1))
-                        #need = header_len-(header_len-header_len_header)+body_len
-                        print '需要接受的数据长度为 %d' %  need
-                        print "第一次接收中的有效数据为%d" % body_in_header_len
-                        #again_len = body_len-body_in_header_len
-                        #print "准备接受的数据长度为%d" %  again_len
-                        while 1:
-                            #need_len = again_len+header_len
-                            #print "已获取长度为 %d" % (len(buf))
-                            #print '再收一次'
-                            cache = cl.recv(1024)
-                            #if len(cache) < 1024:
-                            #    return buf
-                            buf = buf+cache
-                            #带有长度的长连接，需要用实际长度来判断接收完毕
-                            if need == len(buf):
-                                return buf
-                            if len(cache) == 0:
-                                return buf
-                            #buf = cl.recv(again_len)+buf
-            #cbuf1=cl.recv(cbufLen,socket.MSG_WAITALL)
-                        #print '已接收长度 %d'  % (len(cbuf_data))
-                            #print '接收所有数据成功'
-                            #print '所有数据长度为 %d  %d' % (len(buf),len(cache))
-                    else:
-                        print '不包含length字段，无法处理请求!'
-                        cl.close
-                        return 1
+                    #need = proxy_first_data_len-(proxy_first_data_len-proxy_proxy_first_data_len)+body_len
+                    print '需要接受的数据长度为 %d' %  need
+                    print "第一次接收中的有效数据为%d" % proxy_first_body_len
+                    #again_len = body_len-body_in_proxy_first_data_len
+                    #print "准备接受的数据长度为%d" %  again_len
+                    while 1:
+                        #need_len = again_len+proxy_first_data_len
+                        #print "已获取长度为 %d" % (len(buf))
+                        #print '再收一次'
+                        cache = cl.recv(1024)
+                        #if len(cache) < 1024:
+                        #    return buf
+                        buf = buf+cache
+                        #带有长度的长连接，需要用实际长度来判断接收完毕
+                        if need == len(buf):
+                            return buf
+                        if len(cache) == 0:
+                            return buf
+                        #buf = cl.recv(again_len)+buf
+        #cbuf1=cl.recv(cbufLen,socket.MSG_WAITALL)
+                    #print '已接收长度 %d'  % (len(cbuf_data))
+                        #print '接收所有数据成功'
+                        #print '所有数据长度为 %d  %d' % (len(buf),len(cache))
                 except Exception,ex:
                     print Exception,":",ex
                     cl.close()
@@ -154,7 +159,7 @@ def forward(hostip,headers):
             while 1:
                 try:
                     #cl.settimeout(1)
-                    #need_len = again_len+header_len
+                    #need_len = again_len+proxy_first_data_len
                     #print "已获取长度为 %d" % (len(buf))
                     cache = cl.recv(1024)
                     #print "多次接收时cache 长度为%d" % (len(cache))
